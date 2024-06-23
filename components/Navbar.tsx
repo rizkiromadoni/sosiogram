efe2e40dac0b9c61773aa61c0fc6ebc717809942
rizkiromadoni <coradex06@gmail.com>
2024-06-23 12:05:15 +0700

    feat: authentications

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,7 +1,9 @@
 import Link from 'next/link'
 import React from 'react'
 import MobileMenu from './MobileMenu'
-import { CircleFadingPlus, House, Users } from 'lucide-react'
+import { Bell, CircleFadingPlus, House, MessageSquare, Users } from 'lucide-react'
+import { ClerkLoaded, ClerkLoading, SignInButton, UserButton } from '@clerk/nextjs'
+import { SignedIn, SignedOut } from '@clerk/nextjs'
 
 const Navbar = () => {
   return (
@@ -30,6 +32,28 @@ const Navbar = () => {
         </div>
 
         <div className='w-[30%] flex items-center gap-4 xl:gap-8 justify-end'>
+            <ClerkLoading>
+                <div className="inline-block h-4 w-4 animate-spin rounded-full border-4 border-solid border-current border-e-transparent align-[-0.125em] text-surface motion-reduce:animate-[spin_1.5s_linear_infinite] dark:text-white" />
+            </ClerkLoading>
+            <ClerkLoaded>
+                <SignedIn>
+                    <div className='cursor-pointer'>
+                        <Users width={20} height={20} />
+                    </div>
+                    <div className='cursor-pointer'>
+                        <MessageSquare width={20} height={20} />
+                    </div>
+                    <div className='cursor-pointer'>
+                        <Bell width={20} height={20} />
+                    </div>
+                    <UserButton />
+                </SignedIn>
+                <SignedOut>
+                    <Link href="/sign-in" className='text-blue-500'>
+                        Login
+                    </Link>
+                </SignedOut>
+            </ClerkLoaded>
             <MobileMenu />
         </div>
     </div>
---
+++ middleware.ts
@@ -0,0 +1,12 @@
+import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
+
+const protectedRoutes = createRouteMatcher(["/settings(.*)"])
+
+export default clerkMiddleware((auth, req) => {
+    if (protectedRoutes(req))
+        auth().protect()
+});
+
+export const config = {
+  matcher: ["/((?!.*\\..*|_next).*)", "/", "/(api|trpc)(.*)"],
+};
