10011da4c098885872dbd36f89be6e8d79508554
rizkiromadoni <coradex06@gmail.com>
2024-06-25 05:18:41 +0700

    feat: profile page

--- app/profile/[id]/page.tsx
+++ app/profile/[id]/page.tsx
@@ -11,6 +11,38 @@ const ProfilePage = () => {
       </div>
       <div className="w-full lg:w-[70%] xl:w-[50%]">
         <div className="flex flex-col gap-6">
+          <div className="flex flex-col items-center justify-center">
+            <div className="w-full h-64 relative">
+              <Image
+                src="https://images.pexels.com/photos/26152779/pexels-photo-26152779/free-photo-of-kayu-pemandangan-lanskap-lansekap.png"
+                alt=""
+                fill
+                className="object-cover rounded-md"
+              />
+              <Image
+                src="https://images.pexels.com/photos/15301144/pexels-photo-15301144/free-photo-of-pantai-ombak-gelombang-pesisir.jpeg"
+                alt=""
+                width={128}
+                height={128}
+                className="w-32 h-32 rounded-full absolute left-0 right-0 m-auto -bottom-16 ring-4 ring-white object-cover"
+              />
+            </div>
+            <h1 className="mt-20 mb-4 text-2xl font-medium">John Doe</h1>
+            <div className="flex items-center justify-center gap-12 mb-4">
+              <div className="flex flex-col items-center">
+                <span className="font-medium">12</span>
+                <span className="text-sm">Posts</span>
+              </div>
+              <div className="flex flex-col items-center">
+                <span className="font-medium">3.3K</span>
+                <span className="text-sm">Followers</span>
+              </div>
+              <div className="flex flex-col items-center">
+                <span className="font-medium">323</span>
+                <span className="text-sm">Following</span>
+              </div>
+            </div>
+          </div>
           <Feed />
         </div>
       </div>
@@ -18,7 +50,7 @@ const ProfilePage = () => {
         <RightMenu userId="test" />
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default ProfilePage
+export default ProfilePage;
